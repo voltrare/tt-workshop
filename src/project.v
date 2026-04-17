7ccec8ec5067bc9e065fdbd1a5b571af6de41951
voltrare <30526233+voltrare@users.noreply.github.com>
2026-04-17 12:23:30 +0800

    challenge 1

--- src/project.v
+++ src/project.v
@@ -16,12 +16,13 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // 4-bit multiply circuit: ui_in[7:4] * ui_in[3:0] -> 8-bit output
-  assign uo_out  = ui_in[7:4] * ui_in[3:0];
+  // All output pins must be assigned. If not used, assign to 0.
+  assign uo_out[0]  = ( ~ (ui_in[0] ^ ui_in[1]) ^ ui_in[2]);  // Example: ou_out is the sum of ui_in and uio_in
+  assign uo_out[7:1] = 7'b0;
   assign uio_out = 0;
   assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, uio_in, 1'b0};
+  wire _unused = &{ui_in[7:3], uio_in[7:0], ena, clk, rst_n, 1'b0};
 
 endmodule
